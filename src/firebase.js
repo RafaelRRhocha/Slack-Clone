c715af90d1fcbb0aecf3cde822a98337444ab818
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-07-25 16:18:52 -0300

    feat: create setup for the app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBBFV1KZ5kJJsno8V-YDv95gSC32hwJ2kI",
+  authDomain: "slack-clone-67c66.firebaseapp.com",
+  projectId: "slack-clone-67c66",
+  storageBucket: "slack-clone-67c66.appspot.com",
+  messagingSenderId: "690413291823",
+  appId: "1:690413291823:web:2e59de72ab85e70eec39da",
+  measurementId: "G-40TD0WBWB1"
+};
---
+++ src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Login() {
+  return (
+    <h1>sdhasudhas</h1>
+  );
+}
