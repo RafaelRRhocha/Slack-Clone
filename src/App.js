5bbd8630fcaf1ad877b1010f88182798e0489979
Rafael <1rafaelrocha2@gmail.com>
2022-08-01 10:24:02 -0300

    feat: :white_check_mark: create NotFound Route and Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './Main.css';
 import { Login } from './pages/Login';
+import { NotFound } from './pages/NotFound';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </div>
   );
