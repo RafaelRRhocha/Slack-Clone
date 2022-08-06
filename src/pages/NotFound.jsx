a1e5d1bfbe198ff0063029d3d86a4c750abed8cb
Rafael <1rafaelrocha2@gmail.com>
2022-08-06 12:32:06 -0300

    feat: :rocket: finish not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="bg-[#1a1d21] text-white h-[100vh] flex flex-col items-center pt-[10%] gap-4">
+      <h1 className="text-[30px] font-semibold">Page Not Found</h1>
+      <Link to="/">
+        <button className="btn btn-outline btn-secondary">Back To Home Page</button>
+      </Link>
+    </div>
   )
 }
